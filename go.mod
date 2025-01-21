5f366a24752de67f90785482a1f980a9756035ed
cooler-SAI <cooler19860212@yahoo.com>
2025-01-21 21:37:04 +0300

    added zerolog to repo

--- go.mod
+++ go.mod
@@ -6,6 +6,10 @@ require (
 	github.com/cespare/xxhash/v2 v2.2.0 // indirect
 	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
 	github.com/go-redis/redis v6.15.9+incompatible // indirect
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.19 // indirect
 	github.com/redis/go-redis v6.15.9+incompatible // indirect
 	github.com/redis/go-redis/v9 v9.7.0 // indirect
+	github.com/rs/zerolog v1.33.0 // indirect
+	golang.org/x/sys v0.12.0 // indirect
 )
--- main.go
+++ main.go
@@ -4,13 +4,16 @@ import (
 	"context"
 	"errors"
 	"fmt"
-	"log"
 	"time"
 
 	"github.com/redis/go-redis/v9"
+	"github.com/rs/zerolog"
+	"github.com/rs/zerolog/log"
 )
 
 func main() {
+	zerolog.TimeFieldFormat = zerolog.TimeFormatUnix
+	log.Logger = log.Output(zerolog.ConsoleWriter{Out: zerolog.NewConsoleWriter().Out})
 
 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
@@ -21,29 +24,36 @@ func main() {
 	defer func(rdb *redis.Client) {
 		err := rdb.Close()
 		if err != nil {
-			panic(err)
+			log.Fatal().Err(err).Msg("Error closing Redis connection")
 		}
 	}(rdb)
 
 	err := rdb.Set(ctx, "key", "hello, Redis!", 0).Err()
 	if err != nil {
-		log.Fatalf("Error setting key: %v", err)
+		log.Fatal().Err(err).Msg("Error setting key in Redis")
+	} else {
+		log.Info().Msg("Successfully set key in Redis")
 	}
 
 	val, err := rdb.Get(ctx, "key").Result()
 	if err != nil {
-		log.Fatalf("Error getting key: %v", err)
+		log.Fatal().Err(err).Msg("Error getting key from Redis")
+	} else {
+		log.Info().Str("key", "key").Str("value", val).
+			Msg("Successfully retrieved key from Redis")
 	}
 	fmt.Println("key:", val)
 
 	val2, err := rdb.Get(ctx, "nonexistent").Result()
 	if !errors.Is(err, redis.Nil) {
 		if err != nil {
-			log.Fatalf("Error getting nonexistent key: %v", err)
+			log.Fatal().Err(err).Msg("Error getting nonexistent key from Redis")
 		} else {
-			fmt.Println("nonexistent key:", val2)
+			log.Info().Str("key", "nonexistent").Str("value", val2).
+				Msg("Nonexistent key retrieved")
 		}
 	} else {
+		log.Warn().Msg("Key does not exist in Redis")
 		fmt.Println("key does not exist")
 	}
 }
